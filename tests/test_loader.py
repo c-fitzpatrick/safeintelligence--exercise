83603029cfb5c22524ee2acf854d724478c5b11a
c-fitzpatrick <christopher.fitzpatrick@mindshareworld.com>
2024-11-19 07:53:46 +0000

    Parameterize test for SingleProcessDataset

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,7 +1,7 @@
 import torch
 
 import pytest
-
+import pathlib
 
 from provided.loader import SingleProcessDataset, MultiProcessDataset
 from provided.constants import DATA_DIR
@@ -9,12 +9,25 @@ from provided.constants import DATA_DIR
 
 class TestLoader:
 
-    def test_single_loader(self):
-        csv_file = DATA_DIR / "3d_data.csv"
+    @pytest.mark.parametrize(
+        "csv_file, n",
+        [
+            (DATA_DIR / "3d_data.csv", 10000),
+            (DATA_DIR / "3d_data_10.csv", 10),
+        ],
+    )
+    def test_single_loader(self, csv_file: pathlib.Path, n: int):
+        """
+        Test SingleProcessDataset. Confirm that features and labels are of the correct type and shape.
+        """
         dataset = SingleProcessDataset(csv_file)
 
-        assert dataset.features.dtype == torch.float32, "Features should be of type torch.float32"
-        assert dataset.labels.dtype == torch.int64, "Labels should be of type torch.int64"
+        assert (
+            dataset.features.dtype == torch.float32
+        ), "Features should be of type torch.float32"
+        assert (
+            dataset.labels.dtype == torch.int64
+        ), "Labels should be of type torch.int64"
 
-        assert dataset.features.shape == (10000, 3), "Features should have shape (10000, 3)"
-        assert dataset.labels.shape == (10000,), "Labels should have shape (10000,)"
+        assert dataset.features.shape == (n, 3), f"Features should have shape ({n}, 3)"
+        assert dataset.labels.shape == (n,), f"Labels should have shape ({n},)"
